8eaaf779c57154e8af468d4ad93ae474d30c0ae0
Kocayamak <mikailkocayamak@gmail.com>
2023-02-14 22:46:53 +0300

    updated fetch link in server side

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -19,13 +19,16 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch("http://localhost:8080/api/v1/dalle", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ prompt: form.prompt }),
-        });
+        const response = await fetch(
+          "https://dall-e-byxs.onrender.com/api/v1/dalle",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ prompt: form.prompt }),
+          }
+        );
 
         const data = await response.json();
         setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
@@ -42,30 +45,30 @@ const CreatePost = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (form.prompt && form.photo){
+    if (form.prompt && form.photo) {
       setLoading(true);
 
-      try{
-        const response = await fetch("http://localhost:8080/api/v1/posts", {
-          method : "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(form),
-        });
+      try {
+        const response = await fetch(
+          "https://dall-e-byxs.onrender.com/api/v1/posts",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(form),
+          }
+        );
 
         await response.json();
 
         navigate("/");
-      }
-      catch(error){
+      } catch (error) {
         alert(error);
-      }
-      finally{
+      } finally {
         setLoading(false);
       }
-    }
-    else {
+    } else {
       alert("Please enter a prompt and generate an image");
     }
   };
